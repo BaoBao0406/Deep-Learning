233c217a70601f82167e58cf6ac50d801a60bd9d
BaoBao0406 <46430379+BaoBao0406@users.noreply.github.com>
2020-05-30 16:11:15 +0800

    Update

---
+++ Deep Learning 2 NLP Basic theory Book3/Ch.6 LSTM.py
@@ -0,0 +1,24 @@
+class LSTM:
+    def __init__(self, Wx, Wh, b):
+        self.params = [Wx, Wh, b]
+        self.grads = [np.zeros_like(Wx), np.zeros_like(Wh), np.zeros_like(b)]
+        self.cache = None
+    
+    def forward(self, x, h_prev, c_prev):
+        Wx, Wh, b = self.params
+        N, H = h_prev.shape
+        
+        A = np.dot(x, Wx) + np.dot(h_prev, Wh) + b
+        
+        f = A[:, :H]
+        g = A[:, H:2*H]
+        i = A[:, 2*H:3*H]
+        o = A[:, 3*H:]
+        
+        c_next = f * c_prev + g * i
+        h_next = o * np.tanh(c_next)
+        
+        self.cache = (x, h_prev, c_prev, i, f, g, o, c_next)
+        return h_next, c_next
+    
+    
---
+++ Deep Learning 2 NLP Basic theory Book3/Ch.7 peeky_seq2seq.py
@@ -0,0 +1,50 @@
+class PeekyDecoder:
+    def __init__(self, vocab_size, wordvec_size, hidden_size):
+        V, D, H = vocab_size, wordvec_size, hidden_size
+        rn = np.random.randn
+        
+        embed_W = (rn(V, D) / 100).astype('f')
+        lstm_Wx = (rn(H + D, 4 * H) / np.sqrt(H + D)).astype('f')
+        lstm_Wh = (rn(H, 4 * H) / np.sqrt(H)).astype('f')
+        lstm_b = np.zerros(4 * H).astype('f')
+        affine_W = (rn(H + H, V) / np.sqrt(H + H)).astype('f')
+        affine_b = np.zeros(V).astype('f')
+        
+        self.embed = TimeEmbedding(embed_W)
+        self.lstm = TimeLSTM(lstm_Wx, lstm_Wh, lstm_b, stateful=True)
+        self.affine = TimeAffine(affine_W, affine_b)
+        
+        self.params, self.grads = [], []
+        for layer in (self.embed, self.lstm, self.affine):
+            self.params += layer.params
+            self.grads += layer.grads
+        self.cache = None
+    
+    def forward(self, xs, h):
+        N, T = xs.shape
+        N, H = h.shape
+        
+        self.lstm.set_state(h)
+        
+        out = self.embed.forward(xs)
+        hs = np.repeat(h, T, axis=0).resahpe(N, T, H)
+        out = np.concatenate((hs, out), axis=2)
+        
+        out = self.lstm.forwarrd(out)
+        out = np.concatenate((hs, out), axis=2)
+        
+        score = self.affine.forward(out)
+        self.cache = H
+        return score
+
+class PeekySeq2seq(Seq2seq):
+    def __init__(self, vocab_size, wordvec_size, hidden_size):
+        V, D, H = vocab_size, wordvec_size, hidden_size
+        self.encoder = Encoder(V, D, H)
+        self.decoder = PeekyDecoder(V, D, H)
+        self.softmax = TimeSoftmaxWithLoss()
+        
+        self.params = self.encoder.params + self.decoder.params
+        self.grads = self.encoder.grads + self.decoder.grads
+    
+    
